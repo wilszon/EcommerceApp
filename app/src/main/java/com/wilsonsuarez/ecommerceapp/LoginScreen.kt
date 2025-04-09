e495fd0894e8392dc9a92c2271c3f91d7013eb32
Wilson Suarez <wsuarez874@unab.edu.co>
2025-04-09 08:49:09 -0500

    Diseño Login Screen

--- app/src/main/java/com/wilsonsuarez/ecommerceapp/LoginScreen.kt
+++ app/src/main/java/com/wilsonsuarez/ecommerceapp/LoginScreen.kt
@@ -1,6 +1,7 @@
 package com.wilsonsuarez.ecommerceapp
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,10 +12,14 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,14 +30,17 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+
 @Composable
-fun LoginScreen(){
+fun LoginScreen() {
     Scaffold { innerPadding ->
         Column(
-            modifier = Modifier.padding(innerPadding)
+            modifier = Modifier
+                .padding(innerPadding)
                 .fillMaxSize()
                 .padding(horizontal = 30.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
             Image(
                 painter = painterResource(R.drawable.logo_unab),
@@ -49,7 +57,7 @@ fun LoginScreen(){
                 fontWeight = FontWeight.Bold
             )
 
-            Spacer(modifier = Modifier.height(28.dp))
+            Spacer(modifier = Modifier.height(24.dp))
 
             OutlinedTextField(
                 value = "",
@@ -68,12 +76,51 @@ fun LoginScreen(){
                 shape = RoundedCornerShape(12.dp)
             )
 
+            Spacer(modifier = Modifier.height(24.dp))
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = "Email",
+                        tint = Color(0xFFFF9900)
+                    )
+                },
+                label = {
+                    Text(text = "Contraseña")
+                },
+                shape = RoundedCornerShape(12.dp)
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = {}, modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                shape = RoundedCornerShape(12.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFFF9900)
+                )
+            ) {
+                Text("Iniciar Sesión")
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            TextButton(onClick = {}) {
+                Text(text = "¿No tienes una cuenta? Registrate",
+                    color = Color(0xFFFF9900))
+            }
         }
     }
 }
 
 @Preview
 @Composable
-fun LoginScreenPreview(){
+fun LoginScreenPreview() {
     LoginScreen()
 }
